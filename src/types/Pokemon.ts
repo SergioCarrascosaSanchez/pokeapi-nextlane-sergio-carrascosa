3b98dccd65a893ba58bedde5f617c16998f5c7b1
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2025-06-06 07:04:14 +0200

    fix: removes unused pokemon propieties

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -1,22 +1,13 @@
+import { PokemonTypes } from "./PokemonTypes";
+
 export interface Pokemon {
   sprites: {
     front_default: string;
   };
-  moves: {
-    move: {
-      name: string;
-    };
-  }[];
   types: {
     slot: number;
     type: {
-      name: string;
-    };
-  }[];
-  stats: {
-    base_stat: number;
-    stat: {
-      name: string;
+      name: PokemonTypes;
     };
   }[];
 }
