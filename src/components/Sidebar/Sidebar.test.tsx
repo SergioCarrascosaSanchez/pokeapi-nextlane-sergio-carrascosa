9f4ac68e81884efa31c10e63917271ea493a5b28
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2025-06-06 08:12:06 +0200

    feat: improve sidebar responsive

--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import { Sidebar } from "./Sidebar";
 import { PokemonSummary } from "@/types/PokemonSummary";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("../SidebarItem/SidebarItem", () => ({
   SidebarItem: ({ pokemon }: { pokemon: PokemonSummary }) => (
@@ -24,10 +25,26 @@ describe("Sidebar", () => {
   it("should render the logo and a list of Pokémon items", () => {
     render(<Sidebar />);
 
-    const image = screen.getByAltText("Descripción de mi imagen optimizada");
+    const image = screen.getByAltText("PokéAPI logo");
     expect(image).toBeInTheDocument();
 
     expect(screen.getByText("bulbasaur")).toBeInTheDocument();
     expect(screen.getByText("charmander")).toBeInTheDocument();
+  });
+
+  it("should toggle Pokémon list on burger menu click", async () => {
+    const user = userEvent.setup();
+    render(<Sidebar />);
+
+    const list = screen.getByTestId("sidebar-list");
+    const burgerMenu = screen.getByAltText("Toggle menu");
+
+    expect(list).not.toHaveClass("open");
+
+    await user.click(burgerMenu);
+    expect(list).toHaveClass("open");
+
+    await user.click(burgerMenu);
+    expect(list).not.toHaveClass("open");
   });
 });
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { usePokemonSummaryList } from "@/hooks/usePokemonSummaryList";
 import { SidebarItem } from "../SidebarItem/SidebarItem";
 import "./Sidebar.css";
@@ -6,17 +7,31 @@ import Image from "next/image";
 
 export function Sidebar() {
   const { pokemons } = usePokemonSummaryList();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="sidebar">
-      <Image
-        src="/pokeapi.png"
-        alt="Descripción de mi imagen optimizada"
-        width={185}
-        height={105}
-      />
-      {pokemons.map((pokemon) => (
-        <SidebarItem key={pokemon.name} pokemon={pokemon} />
-      ))}
+      <div className="sidebar-header">
+        <Image src="/pokeapi.png" alt="PokéAPI logo" width={185} height={105} />
+
+        <Image
+          src="/burger-menu.svg"
+          alt="Toggle menu"
+          width={50}
+          height={50}
+          onClick={() => setIsOpen(!isOpen)}
+          className="sidebar-toggle"
+        />
+      </div>
+
+      <div
+        className={`sidebar-list ${isOpen ? "open" : ""}`}
+        data-testid="sidebar-list"
+      >
+        {pokemons.map((pokemon) => (
+          <SidebarItem key={pokemon.name} pokemon={pokemon} />
+        ))}
+      </div>
     </div>
   );
 }
