34dd6cf0cd555836e30c6f7a3d8790a4df35b058
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2025-06-06 07:03:20 +0200

    feat: improves PokemonDetail

--- src/components/PokemonDetail/PokemonDetail.tsx
+++ src/components/PokemonDetail/PokemonDetail.tsx
@@ -1,24 +1,55 @@
 "use client";
-import { capitalize } from "@/helpers/capitalize";
-import { usePokemon } from "@/hooks/usePokemon";
 import { useSelectedPokemon } from "@/hooks/useSelectedPokemon";
+import { usePokemon } from "@/hooks/usePokemon";
+import { capitalize } from "@/helpers/capitalize";
 import Image from "next/image";
+import "./PokemonDetail.css";
+import { Loader } from "../Loader/Loader";
+import { TypeChip } from "../TypeChip/TypeChip";
 
 export function PokemonDetail() {
   const { selectedPokemon } = useSelectedPokemon();
+
   const { loading, pokemon, error } = usePokemon({
     name: selectedPokemon?.name,
   });
-  if (!pokemon || !selectedPokemon) return <p>No pokemon selected</p>;
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
+
+  if (loading) {
+    return (
+      <div className="pokemon-detail-container">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (!selectedPokemon || !pokemon) {
+    return (
+      <div className="pokemon-detail-container">
+        <p>No Pokémon selected</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="pokemon-detail-container">
+        <p>{error}</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="pokemon-detail-container">
       <h1>{capitalize(selectedPokemon.name)}</h1>
+      <div className="pokemon-types-container">
+        {pokemon.types.map((type) => (
+          <TypeChip key={type.type.name} type={type.type.name} />
+        ))}
+      </div>
       <Image
         src={pokemon.sprites.front_default}
-        width={500}
-        height={500}
+        width={300}
+        height={300}
         alt={`${selectedPokemon.name}-sprite`}
       />
     </div>
